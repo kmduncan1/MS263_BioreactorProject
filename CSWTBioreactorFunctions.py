1c867277cb003f14b3950e8823ef84207c91c479
Katie Duncan <katherine.duncan@sjsu.edu>
2024-05-08 12:02:46 -0700

    Major updates for PCA to CSWT module and updates to project notebook

--- CSWTBioreactorFunctions.py
+++ CSWTBioreactorFunctions.py
@@ -152,3 +152,68 @@ def dfclean_nitratetemp (dataframe,var='nitrate'):
     ii_positive = (0<=dffinite[var]) & (0<=dffinite['temp'])
     dfcleanednt = dffinite[ii_positive]
     return(dfcleanednt)
+
+def BioreactorPCA(df,varlist=['temp','cond','sal','nitrate','hdo','sat','phv','ph']):
+    '''Function to calculate eigenvalues, eigenvectors, principle component scores, percent variance accounted for by PC1 and PC2, PC factor loading matrix, and PC scores from a list of variables for a given pandas dataframe.
+        Parameters
+    -------------
+    df: dataframe
+        The path from the workspace containing the module to the HyrdaData folder excluding the file name.
+        Example: '.../MS263_Project/HydraData/'
+        Defaults to 'HydraData/' assuming HydraData folder is stored in the same folder as the module for this package.
+    varlist: list
+        List of variables to use for PCA. Defaults to short_vars = ['temp','cond','sal','nitrate','hdo','sat','phv','ph'].
+
+    Returns
+    -------------
+    datamatrix_zscore, evalues_sorted, evectors_sorted, PC1_pvar, PC2_pvar, FactorMatrix, PCscores, TimeMatrix
+    datamatrix_zscore: matrix
+        Matrix of z-scores for the data matrix.
+    evalues_sorted: matrix
+        Matrix of eigenvalues sorted by highest to least variance accounted for by PC.
+    evectors_sorted: matrix
+        Matrix of eigenvectors sorted by highest to least variance accounted for by PC.
+    PC1_pvar: float
+        Percentage of variance in the data accounted for by PC1.
+    PC2_pvar: dataframe
+        Percentage of variance in the data accounted for by PC2.
+    FactorMatrix: matrix
+        Factor loading matrix for the data matrix with PC's sorted so that ordering follows PC1, PC2,...PCn by column.
+    PCscores: matrix
+        Matrix of principle component scores sorted so that ordering follows PC1, PC2,...PCn by column.
+    TimeMatrix: matrix
+        Matrix of UTC times for the given dataframe to be used for plotting principle component score timeseries.
+    '''
+    #Import packages and modules
+    import pandas as pd
+    import numpy as np
+    from scipy import linalg
+    
+    dfmatrixscaffold = pd.DataFrame() #Create a scaffold to hold values for creating a matrix
+    #Populate matrix with values for variables from the given dataframe from  varlist
+    for x in varlist:
+       dfmatrixscaffold[x] = df[x]
+    
+    datamatrix = dfmatrixscaffold.values  #Create datamatrix from values contained in dfmatrixscaffold
+    datamatrix_zscore = (datamatrix- np.mean(datamatrix, axis=0))/np.std(datamatrix,axis=0, ddof=1) #Calculate z-scores from the data matrix
+    
+    corr_matrix = np.cov(datamatrix_zscore, rowvar=False) #Create a correlation matrix
+    evalues, evectors = linalg.eig(corr_matrix) #Calculate eigenvalues and eigenvectors
+    
+    #Sort eigenvalues and eigenvectors by decreasing variance (PC1 first, PC2 second, etc)
+    idx = evalues.argsort()[::-1]  
+    evalues_sorted = evalues[idx]
+    evectors_sorted = evectors[:,idx]
+
+    realvalues = np.real(evalues_sorted) #Convert eigenvalues to real numbers 
+    percent_variance = realvalues/np.sum(realvalues)*100 #Create a matrix with the percentage of variables PC's account for
+    PC1_pvar = percent_variance[0] #Extract percent variance for PC1
+    PC2_pvar = percent_variance[1] #Repeat for PC2
+
+    LambdaMatrix = np.diag(realvalues) #Create a lambda matrix with eigenvalues distributed diagonally
+    FactorMatrix = evectors@(LambdaMatrix**0.5) #Create a factor loading matrix
+    
+    PCscores = datamatrix_zscore@evectors_sorted #Calculate principle component scores
+    TimeMatrix = df['utc_time'].values  #Creates a time matrix for plotting PC scores
+    
+    return(datamatrix_zscore, evalues_sorted, evectors_sorted, PC1_pvar, PC2_pvar, FactorMatrix, PCscores, TimeMatrix)
